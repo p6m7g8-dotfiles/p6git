e25fc1bb9666c1fbd657810b8f3878837bec3a7e
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-07-17 09:19:06 -0400

    fix: chases callback argument order change

--- init.zsh
+++ init.zsh
@@ -19,7 +19,8 @@ p6df::modules::p6git::deps() {
 #>
 ######################################################################
 p6df::modules::p6git::init() {
-    local dir="$1"
+    local _module="$1"
+    local dir="$2"
 
     p6_bootstrap "$dir"
 }
